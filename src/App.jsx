9c5c0d39cb560846eccd00a4c0f398ae0183b4df
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-10-09 23:44:57 -0300

    feat: configurações iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+import TaskBox from './components/TaskBox/TaskBox'
+
+function App() {
+ 
+  return (
+    <>
+      <Header />
+      <TaskBox />
+    </>
+  )
+}
+
+export default App
