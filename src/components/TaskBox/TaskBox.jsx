6f4a2546cc59a69368bde17f4f9da16639c8c9c1
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-10-10 23:00:18 -0300

    feat: lógica de renderização do TaskBox

--- src/components/TaskBox/TaskBox.jsx
+++ src/components/TaskBox/TaskBox.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
+import TaskCard from "../TaskCard/TaskCard"
 import style from "./TaskBox.module.css"
 
-function TaskBox(){
+function TaskBox({listTasks, setDeleteKey}){
+    useEffect(()=>{},[listTasks])
+
     return (
         <main>
-            
+            {listTasks.map((task)=> <TaskCard taskTitle={task.title} key={task.id} setDeleteKey={setDeleteKey} keyValue={task.id} /> )}
         </main>
     )
     
