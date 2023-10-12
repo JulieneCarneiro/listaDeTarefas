2cfb2641a0353e3be6db3b7b724988bbf6a817e8
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-10-12 20:06:39 -0300

    feat: lógica de seleção

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import Button from "../Button/Button";
 import style from "./Search.module.css";
 
-function Search({setTask}) {
+function Search({setTask, setSelected, selected}) {
     const [valor, setValor] = useState("")
     const [contador, setContador] = useState(0)
+    const [clean, setClean] = useState(false)
+    
 
     function handleChange(event) {
         setValor(event.target.value)
@@ -14,19 +16,33 @@ function Search({setTask}) {
         if (valor.length > 0 && valor.length < 64){
             setTask({
                 id:contador,
-                title:valor
+                title:valor,
+                select:valor == selected
             })
             setContador(contador + 1)
         } else {
             alert("Número de caracteres inválidos! O título da sua tarefa deve ter entre 1 a 64 caracteres.")
         }
     }
+
+    function searchButton(){
+        if (clean){
+            setSelected("")
+            setClean(false)
+        } else if (valor.length > 0 && valor.length < 64){
+            setSelected(valor)
+            setClean(true)
+        } else {
+            alert("Número de caracteres inválidos! O título da sua tarefa deve ter entre 1 a 64 caracteres.")
+        }
+    }
+    
     return (
         <div className={style.searchBox}>
-            <input type="text" placeholder="Busque sua tarefa" value={valor} onChange={handleChange} />
+            <input type="text" placeholder="Busque sua tarefa" value={valor} onChange={handleChange}/>
             <div className={style.buttonBox}>
                 <Button title="Criar" width="45%" margin="5px 0" click={createButton}/>
-                <Button title="Pesquisar" width="45%" margin="5px 0" />
+                <Button title={clean ? "Limpar pesquisa" : "Pesquisar"} width="45%" margin="5px 0" click={searchButton}/>
             </div>
         </div>
     );
